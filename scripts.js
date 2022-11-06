ab5901919b99c151d7ac56a9a8e748d4918de3ad
stewart8181 <92873065+stewart8181@users.noreply.github.com>
2022-11-06 23:33:32 +0000

    Basic files for web page. Drawn board on screen.

---
+++ scripts.js
@@ -0,0 +1,8 @@
+let playerOne = prompt("Enter player one name: ");
+let playerTwo = prompt("Enter player two name: ");
+
+document.getElementById('player_one').innerHTML = playerOne;
+document.getElementById('player_two').innerHTML = playerTwo;
+
+document.getElementById('player_turn').innerHTML = playerOne;
+
